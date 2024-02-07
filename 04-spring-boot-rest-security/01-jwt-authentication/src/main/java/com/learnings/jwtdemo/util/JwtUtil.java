f39c5a3fe9ebdf366ac321d1d945a6023a308d48
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2024-02-07 23:30:06 +0530

    learning jwt: modified code

--- 04-spring-boot-rest-security/01-jwt-authentication/src/main/java/com/learnings/jwtdemo/util/JwtUtil.java
+++ 04-spring-boot-rest-security/01-jwt-authentication/src/main/java/com/learnings/jwtdemo/util/JwtUtil.java
@@ -3,9 +3,11 @@ package com.learnings.jwtdemo.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
