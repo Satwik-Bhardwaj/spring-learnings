e0e8b6e9dda4e42170f7f2f8bb5dc6c631588486
Satwik-Bhardwaj <satwikbhardwaj123@gmail.com>
2024-03-08 18:55:43 +0530

    adding msal learning

---
+++ 04-spring-boot-rest-security/02-security-mfa/src/main/java/com/satwik/SecurityMFA/SecurityMfaApplication.java
@@ -0,0 +1,13 @@
+package com.satwik.SecurityMFA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMfaApplication.class, args);
+	}
+
+}
---
+++ 04-spring-boot-rest-security/02-security-mfa/src/test/java/com/satwik/SecurityMFA/SecurityMfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satwik.SecurityMFA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
